d0adba4032109df730bada6f0ba194fe3a575377
Luong Tuan Kiet <kietboy8@gmail.com>
2025-12-23 11:36:59 +0700

    add docker config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Quan trọng: Cho phép Docker map port ra ngoài
+    port: 5173,
+    watch: {
+      usePolling: true // Quan trọng: Giúp Hot Reload hoạt động tốt trên Windows/Docker
+    }
+  }
 })
